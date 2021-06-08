45c3bba9a1ada31441e9d1e3e45a850a9d6f9cda
chocheJos <josue120991@gmail.com>
2021-06-07 22:46:08 -0500

    Delete functions.js

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".menuDesp span").click(function(){
-        $(".menuDesp ul ul").slideUp();
-        if(! $(this).next().is(":visible")){
-            $(this).next().slideDown();
-        }
-    })
-})
